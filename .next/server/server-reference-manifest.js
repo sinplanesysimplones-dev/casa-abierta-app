40466ae4c0173765e2fbd92fbc3b90a4225c9da2
Abner Ro <a2019@192.168.1.47>
2026-07-31 00:35:59 -0500

    Add compiled Next.js build

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"7qeYw4WQCd3xlDMaP9rIRIj6Um7wzeh2Sm6BQfwsEcQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"SVbGoiDGnsGcqfgLuGzodU6NC/lIKNPBpq5xjLTxY84=\"\n}"
