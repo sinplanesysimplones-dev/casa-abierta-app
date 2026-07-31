04b6df9ee0501d80cdcd8a9c1e28aba9e12abfb9
Abner Ro <a2019@192.168.1.47>
2026-07-31 00:33:58 -0500

    Force redeploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,5 +2,6 @@ import '@/src/styles/index.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
+  // SONAR - Professional Archetype Discovery App
   return <Component {...pageProps} />
 }
